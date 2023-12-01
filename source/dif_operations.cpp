4e9af2b4121dbdfdf1151dc8bb40b85ef4036965
nniikon <kolya.kasparov@gmail.com>
2023-12-01 03:31:03 +0300

    fix multiplication bug

--- source/dif_operations.cpp
+++ source/dif_operations.cpp
@@ -9,44 +9,44 @@
 
 #define DIF(node) difDifNode_recursive(tree, (node))
 #define CPY(node) treeCreateNode(tree, (node)->leftBranch, (node)->rightBranch, (node)->parentBranch, (node)->data)
-#define OPR(OPR, left, right, parent) treeCreateNode(tree, (left), (right), (parent), {{(DifOprType)DIF_OPR_##OPR}, DIF_NODE_TYPE_OPR})
+#define OPR(OPR, left, right, parent) treeCreateNode(tree, (left), (right), (parent), {{.opr = (DifOprType)DIF_OPR_##OPR}, DIF_NODE_TYPE_OPR})
 
-double difEvl_add(TreeNode *node, double left, double right)
+double difEvl_add(TreeNode* node, double left, double right)
 {
     return left + right;
 }
 
-TreeNode *difDif_add(Tree *tree, TreeNode *node)
+TreeNode* difDif_add(Tree* tree, TreeNode* node)
 {
     return OPR(ADD, DIF(L), DIF(R), P);
 }
 
-double difEvl_sub(TreeNode *node, double left, double right)
+double difEvl_sub(TreeNode* node, double left, double right)
 {
     return left - right;
 }
 
-TreeNode *difDif_sub(Tree *tree, TreeNode *node)
+TreeNode* difDif_sub(Tree* tree, TreeNode* node)
 {
     return OPR(SUB, DIF(L), DIF(R), P);
 }
 
-double difEvl_mul(TreeNode *node, double left, double right)
+double difEvl_mul(TreeNode* node, double left, double right)
 {
     return left * right;
 }
 
-TreeNode *difDif_mul(Tree *tree, TreeNode *node)
+TreeNode* difDif_mul(Tree* tree, TreeNode* node)
 {
     return OPR(ADD, OPR(MUL, DIF(L), CPY(R), C), OPR(MUL, CPY(L), DIF(R), C), P);
 }
 
-double difEvl_div(TreeNode *node, double left, double right)
+double difEvl_div(TreeNode* node, double left, double right)
 {
     return left / right;
 }
 
-TreeNode *difDif_div(Tree *tree, TreeNode *node)
+TreeNode* difDif_div(Tree* tree, TreeNode* node)
 {
     return OPR(DIV, OPR(SUB, OPR(MUL, DIF(L), CPY(R), nullptr), OPR(MUL, DIF(R), CPY(L), nullptr), nullptr), OPR(MUL, CPY(R), CPY(R), nullptr), nullptr); 
 }
